fb1b5da1be26ac97a4d0f5757246c64412000c4e
Luca Tovoli <lucatovoli@gmail.com>
2018-05-11 20:56:56 +0200

    Initial Commit

---
+++ GOP2018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GOP2018
+//
+//  Created by Luca Antonio Tovoli on 11/05/18.
+//  Copyright © 2018 Luca Antonio Tovoli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
