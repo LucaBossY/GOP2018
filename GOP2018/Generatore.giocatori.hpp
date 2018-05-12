503fcd97c1a0c2d5d1aa0d6536e58086ed700411
LucaBossY <lucatovoli@gmail.com>
2018-05-12 14:52:07 +0200

    Add files via upload
    Nuova funzione per generazione giocatori

---
+++ GOP2018/Generatore.giocatori.hpp
@@ -0,0 +1,24 @@
+//
+//  Generatore.giocatori.hpp
+//  GOP2018
+//
+//  Created by Luca Antonio Tovoli on 11/05/18.
+//  Copyright © 2018 Luca Antonio Tovoli. All rights reserved.
+//
+
+#include <iostream>
+#include <iostream>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+using namespace std;
+
+
+void GeneratoreGiocatori() {
+
+	int giocatori;
+	cout << "Quanti giocatori prenderanno parte alla partita?"
+	cin >> giocatori;
+
+
+}
