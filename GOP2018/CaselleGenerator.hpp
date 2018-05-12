ce798ecca4fdd16f4ddbcd66b14e559503176fb8
Luca Tovoli <lucatovoli@gmail.com>
2018-05-12 09:56:38 +0200

    Add file

---
+++ GOP2018/CaselleGenerator.hpp
@@ -0,0 +1,14 @@
+//
+//  CaselleGenerator.hpp
+//  GOP2018
+//
+//  Created by Luca Antonio Tovoli on 12/05/18.
+//  Copyright © 2018 Luca Antonio Tovoli. All rights reserved.
+//
+
+#ifndef CaselleGenerator_hpp
+#define CaselleGenerator_hpp
+
+#include <stdio.h>
+
+#endif /* CaselleGenerator_hpp */
